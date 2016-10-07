ff39c3d91d1b3db23dd78b6dcf3db7e8a8ff0fb2
redmonnasson <sdorenbos@roc-dev.com>
2016-10-07 11:20:54 +0200

    Add files via upload

---
+++ src/H10/Odracht5.java
@@ -0,0 +1,105 @@
+package H10;
+
+
+
+
+
+
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.Button;
+
+import java.awt.TextField;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Odracht5 extends Applet{
+
+    TextField tekstveld;
+    Button knop;
+    double invoer;
+    double totaal;
+
+    double gemiddelde;
+    int aantalCijvers;
+    boolean geklikt;
+
+
+    @Override
+    public void init() {
+        super.init();
+        tekstveld = new TextField("", 5);
+        add(tekstveld);
+
+        knop = new Button("Ok");
+        add(knop);
+        Knoplisterner kl = new Knoplisterner();
+        knop.addActionListener(kl);
+
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        int x=50;
+        int y=80;
+        if(geklikt){
+
+
+            if ( invoer < 5.5){
+                g.drawString("het cijver is onvoldoende" , x, y);
+
+
+            }
+            else {
+
+                g.drawString("het cijver is voldoende" , x, y);
+            }
+
+
+
+
+            y=y +20;
+            g.drawString("gemiddelde"+ gemiddelde , x, y);
+        }
+
+    }
+
+
+
+    class Knoplisterner implements ActionListener{
+
+
+        public void actionPerformed(ActionEvent e){
+            String s = tekstveld.getText();
+            invoer = Double.parseDouble(s);
+
+            totaal=totaal+ invoer;
+            aantalCijvers++;
+
+            gemiddelde=+totaal/aantalCijvers;
+
+            geklikt= true;
+
+
+
+
+            repaint();
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
